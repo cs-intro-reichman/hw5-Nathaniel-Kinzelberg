ca49a21ab571f65297c7d1722833394f98117b21
Nathaniel Kinzelberg <nathanielkinzelberg@gmail.com>
2024-12-12 03:01:00 -0500

    attempt 8 HW 5

--- Scrabble.java
+++ Scrabble.java
@@ -159,6 +159,7 @@ public class Scrabble {
                 int wordPoints = wordScore(input);
                 score += wordPoints;
                 System.out.println(input + " earned " + wordPoints + " points. Score: " + score + " points");
+				System.out.println();
                 hand = tempHand;
             }
         }
@@ -167,7 +168,6 @@ public class Scrabble {
             System.out.println("Ran out of letters. Total score: " + score + " points");
         } else {
             System.out.println("End of hand. Total score: " + score + " points");
-			System.out.println();
         }
     }
 	// Plays a Scrabble game. Prompts the user to enter 'n' for playing a new hand, or 'e'
